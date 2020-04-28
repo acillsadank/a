7ef42cfec8aa90705bad57e27c2d317c4406dbb8
acillsadank <64443535+acillsadank@users.noreply.github.com>
2020-04-28 21:58:11 +0700

    test auto install

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # install
-Auto Install Debian 9 Debian 10
-OpenSSH: 22
-Dropbear: 442, 443, 990
-SSL: 444
-Squid3: 80, 8080 (batas untuk IP SSH)
-SSL: http: // myip: 81 / ssl.conf
-OpenVPNSSL: http: // myip: 81 / openvpnssl.ovpn
-OpenVPN: TCP 1194 (konfigurasi klien: http: // myip: 81 / client.ovpn )
-badvpn: badvpn-udpgw port 7200
-nginx: 81
+<b><br>OpenSSH : 22
+<br>Dropbear : 143, 443, 990
+<br>SSL : 444
+<br>Squid3 : 80, 8080 (limit to IP SSH)
+<br>SSL: http://myip:81/ssl.conf
+<br>OpenVPNSSL: http://myip:81/openvpnssl.ovpn
+<br>OpenVPN : TCP 1194 (client config : http://myip:81/client.ovpn)
+<br>badvpn : badvpn-udpgw port 7200
 
-Pemasang Otomatis
+Auto-Installer
 
-apt-get update && apt-get upgrade -y && wget https://raw.githubusercontent.com/acillsadank/install/master/install.sh && chmod + x install.sh && ./install.sh
+apt-get update && apt-get upgrade -y && wget https://raw.githubusercontent.com/acillsadank/install/master/install.sh && chmod +x install.sh && bash install.sh
---
+++ menu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Menu
+
+echo -e " 0 menu             : Displays a list of available commands" | lolcat
+echo -e " 1  Edit              : Edit ports" | lolcat
+echo -e " 2  Usernew    : Creating an SSH Account" | lolcat
+echo -e " 3 Trial              : Create a Trial Account" | lolcat
+echo -e " 4 Delet            : Clearing SSH and OpenVPN Account" | lolcat
+echo -e " 5 Cek               : Check User Login" | lolcat
+echo -e " 6 Member      : Check Member SSH and OpenVPN" | lolcat
+echo -e " 7 Restart        : Restart Service dropbear, webmin" | lolcat
+echo -e "                                 squid3, OpenVPN and SSH" | lolcat
+echo -e " 8 Reboot         : reboot VPS" | lolcat
+echo -e " 9 Speedtest   : speedtest VPS" | lolcat
+echo -e " 10 Info             : System Information" | lolcat
+echo -e " 11 About         : Information about auto install script" | lolcat
+echo -e " 12 Exit              : exit Putty/Connecbot/" | lolcat
+echo -e "                                JuiceSSH" | lolcat
+echo -e ""
